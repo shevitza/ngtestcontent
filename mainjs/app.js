d3d0d5d3a494a73bb4e6383aaa708dc634ddde41
shevitza <shevitza@gmail.com>
2017-02-19 11:04:47 +0200

    Commented code removed

--- mainjs/app.js
+++ mainjs/app.js
@@ -12,24 +12,3 @@ app.config(['$routeProvider', function ($routeProvider) {
 
     }]);
 
-//
-//function randomizeMap(myMap) {
-//    var mapKeys = [];
-//    for (var [key, value] of myMap.entries()) {
-//        mapKeys.push(key);
-//    }
-//    mapKeys.sort(function () {
-//        return .5 - Math.random();
-//    });
-//    arr = [];
-//    for (var i = 0; i < mapKeys.length; i++) {
-//        var v = myMap.get(mapKeys[i]);
-//        var t = new Object();
-//        t.key = mapKeys[i].toString();
-//        t.val = v.toString();
-//        arr.push(t);
-//    }
-// 
-//    return arr;
-//    //returns array of objects with properties key:.. and val:...        
-//}
